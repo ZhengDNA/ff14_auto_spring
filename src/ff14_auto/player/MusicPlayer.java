60095fe9651620a8ef2811153c5e1b4f3555a136
陈征 <1007324849@qq.com>
2021-07-10 19:29:27 +0800

    simulate keyboard

--- src/ff14_auto/player/MusicPlayer.java
+++ src/ff14_auto/player/MusicPlayer.java
@@ -1,6 +1,12 @@
 package ff14_auto.player;
 
+import ff14_auto.entity.MusicEntity;
+import ff14_auto.exceptions.MusicNotReadyException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  * @author : 陈征
@@ -8,5 +14,18 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
-public class MusicPlayer {
+public class MusicPlayer extends Robot {
+    @Autowired
+    private MusicEntity musicEntity;
+
+    public MusicPlayer() throws AWTException {
+
+    }
+
+    public void start() throws MusicNotReadyException {
+        if (!musicEntity.ready()){
+            throw new MusicNotReadyException("MusicEntity 异常!");
+        }
+
+    }
 }
