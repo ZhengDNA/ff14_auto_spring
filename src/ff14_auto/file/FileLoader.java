7ce10f4bbb0d344eb3920197bbfa553080714006
陈征 <1007324849@qq.com>
2021-07-10 19:28:42 +0800

    load & check

--- src/ff14_auto/file/FileLoader.java
+++ src/ff14_auto/file/FileLoader.java
@@ -1,8 +1,15 @@
 package ff14_auto.file;
 
 import ff14_auto.entity.MusicEntity;
+import ff14_auto.exceptions.FileEmptyException;
+import ff14_auto.exceptions.NoMatchResolverException;
+import ff14_auto.file.resolver.FileResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author : 陈征
@@ -11,9 +18,42 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FileLoader {
-    @Autowired
-    private MusicEntity musicEntity;
-    public void printSth(){
-        assert musicEntity!=null;
+    private List<FileResolver> resolvers;
+
+    public void setResolvers(List<FileResolver> resolvers) {
+        this.resolvers = resolvers;
+    }
+
+    public void printSth(String a) {
+        System.out.println(a);
+    }
+
+    public void printSth() {
+        assert resolvers != null;
+        for (FileResolver fileResolver : resolvers) {
+            System.out.println(fileResolver);
+        }
+    }
+
+    public void load(String path) throws Exception {
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException("文件未找到! \n路径: " + file.getAbsolutePath());
+        }
+        Reader reader = new FileReader(file);
+        if (!reader.ready()) {
+            throw new FileEmptyException("文件为空! \n路径: " + file.getAbsolutePath());
+        }
+        char c = (char) reader.read();
+        while (c != ' ' && c != '\n') {
+            c = (char) reader.read();
+        }
+        for (FileResolver resolver : resolvers) {
+            if (resolver.check(c)) {
+                resolver.resolve(file);
+                return;
+            }
+        }
+        throw new NoMatchResolverException("没有匹配的解析器! \n路径: " + file.getAbsolutePath());
     }
 }
