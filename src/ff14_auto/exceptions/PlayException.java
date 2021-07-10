ca70920f25eee075e36b8f7734340e2a59d6a3ea
陈征 <1007324849@qq.com>
2021-07-11 03:10:53 +0800

    播放中的错误

---
+++ src/ff14_auto/exceptions/PlayException.java
@@ -0,0 +1,12 @@
+package ff14_auto.exceptions;
+
+/**
+ * @author : 陈征
+ * @date : 2021-07-11 02:58
+ */
+
+public class PlayException extends Exception{
+    public PlayException(String msg){
+        super(msg);
+    }
+}
